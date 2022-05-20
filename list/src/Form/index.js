e4d9621174e50d718e75e4896e878ba96ec45889
Tatsiana1010 <tanya.kratkevich@bk.ru>
2022-05-20 22:52:17 +0300

    project

---
+++ list/src/Form/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import './index.css';
+
+
+export default function Form(){
+    return(
+<form><label for ="task" >Add your task)</label><input type="text" name="add" id="task"></input></form>
+    );
+}
---
+++ list/src/Header/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './index.css';
+
+
+export default function Header(){
+    return(
+        <div className="Head">
+        <h1 className="Text">TO-DO-LIST</h1>
+        </div>
+    );
+}
