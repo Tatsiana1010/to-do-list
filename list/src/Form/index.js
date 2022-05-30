5b9f2d3f21db117257afb3ffec680151649d66cc
Tatsiana1010 <tanya.kratkevich@bk.ru>
2022-05-30 11:46:06 +0300

    хорошего дня

--- list/src/Form/index.js
+++ list/src/Form/index.js
@@ -13,7 +13,9 @@ export default function Form({onAdd}){
     }
     
     return(
-<form><label>Add your task:</label><input className="form1" onChange={onNameChange} type="text" name="add" id="task"></input><button  className="btn btn-primary" onClick={onSubmit} type="submit">send</button></form>
+<form><label className="styletext">Add your task:</label><input className="form1" onChange={onNameChange} type="text" name="add" id="task"></input><button className="btnsend" onClick={onSubmit} type="submit"><svg xmlns="http://www.w3.org/2000/svg" width="15" height="15" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
+  <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z"/>
+</svg>Send</button></form>
 
     );
 }
--- list/src/FormPoisk/index.js
+++ list/src/FormPoisk/index.js
@@ -1,6 +1,6 @@
 
 import React,{useState} from "react";
-
+import './index.css';
 
 
 export default function FormPoisk({ onFind }) {
@@ -16,10 +16,12 @@ export default function FormPoisk({ onFind }) {
     }
     return (
         <form>
-            <label>To find:</label>
-            <input onChange={onFaindChange} value={taskFaind} type="text" name="add">
+            <label className="textfind"> find your task:</label>
+            <input className="inputfind" onChange={onFaindChange} value={taskFaind} type="text" name="add">
             </input>
-            <button onClick={onSubmit}>Search</button>
+            <button className="buttfind"onClick={onSubmit}><svg xmlns="http://www.w3.org/2000/svg" width="13" height="13" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+  <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+</svg>Search</button>
         </form>
     )
 }
